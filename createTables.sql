0bbc236d713559032ff52520dc12804780ecdbfa
JFerrigan <JFerrigan@wisc.edu>
2021-10-10 16:35:46 -0500

    Add sql file to create tables

---
+++ createTables.sql
@@ -0,0 +1,32 @@
+CREATE TABLE `spikedb`.`dbuser` (
+  `uid` INT NOT NULL AUTO_INCREMENT,
+  `firstName` VARCHAR(45) NULL,
+  `lastName` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `password` VARCHAR(45) NULL,
+  `phoneNumber`VARCHAR(45) NULL, #DOES this need to be integer?
+  `address1` VARCHAR(45) NULL,
+  `address2` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NULL,
+  `state1` VARCHAR(45) NULL,
+  `zipcode` VARCHAR(45) NULL, #DOES this need to be integer?
+  `role` VARCHAR(45) NULL,
+  `balance` FLOAT NULL,
+  PRIMARY KEY (`uid`));
+  
+  CREATE TABLE `spikedb`.`properties` (
+  `pid` INT NOT NULL AUTO_INCREMENT,
+  `uid` INT NULL,
+  `oid` INT NULL,
+  `rent` FLOAT NULL,
+  `status` VARCHAR(45) NULL,
+  PRIMARY KEY (`pid`),
+  FOREIGN KEY (`uid`) REFERENCES dbuser(`uid`));
+  
+CREATE TABLE `spikedb`.`maintenencerequests` (
+  `rid` INT NOT NULL AUTO_INCREMENT,
+  `uid` INT NULL,
+  `message` VARCHAR(1000) NULL,
+  `submissionDate` VARCHAR(45) NULL,
+  PRIMARY KEY (`rid`),
+  FOREIGN KEY (`uid`) REFERENCES dbuser(`uid`));
