61dad714013c933f2d295e89b975a33754ea6fd4
ghellenbran2 <ghellenbran2@wisc.edu>
2021-10-10 08:15:20 -0500

    Modified layouts for CreateAccount and Login pages. Will need to go over how to fetch data login data from the backend to confirm correct credentials exist.

--- react-node-app/spike-exercise/src/CreateAccount.js
+++ react-node-app/spike-exercise/src/CreateAccount.js
@@ -1,52 +1,224 @@
 import React, {useState} from "react"; 
 import Axios from 'axios';
-import Button from "react-bootstrap/Button";
-import FormControl from "react-bootstrap/FormControl"
+import { Form, Button, Row, Col} from "react-bootstrap";
 import {withRouter} from 'react-router-dom'
+
+function CreateAccountForm() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    
+    setValidated(true);
+      
+    // Output Caputured Data
+    if (form.checkValidity() === true) {
+      console.log(event.target.elements.firstName.value)
+      console.log(event.target.elements.lastName.value)
+      console.log(event.target.elements.password.value)
+      console.log(event.target.elements.phoneNumber.value)
+      console.log(event.target.elements.email.value)
+      console.log(event.target.elements.address1.value)
+      console.log(event.target.elements.address2.value)
+      console.log(event.target.elements.city.value)
+      console.log(event.target.elements.state1.value)
+      console.log(event.target.elements.zipcode.value)
+
+      Axios.post("http://localhost:3001/CreateAccount",{
+        firstName: this.state.firstName.trim(),
+        lastName: this.state.lastName.trim(),
+        password: this.state.password.trim(),
+        phoneNumber: this.state.phoneNumber.trim(),
+        email: this.state.email.trim(),
+        address1: this.state.address1.trim(),
+        address2: this.state.address2.trim(),
+        city: this.state.city.trim(),
+        state1: this.state.state1.trim(),
+        zipcode: this.state.zipcode.trim()
+        }).then((response) => {
+            this.props.history.push('/Login');
+        });
+      }          
+    };   
+
+  const handleCancel = (event) => {
+      setValidated(true);
+  }
+
+  return (
+      <>
+          <Row>
+          <div>
+              <h1 className="createAccountHeaders">Create Account</h1>
+          </div>
+          </Row>
+          
+          <Row style={{padding: '5%'}}>
+          <div >            
+              <Form noValidate validated={validated} action="RenterHome" onSubmit={handleSubmit} onCancel={handleCancel}>
+                  <h4 className="createAccountLabels mb-3">Personal Information:</h4>
+                  <Form.Group as={Row} className="mb-3" controlId="validationFirstName">
+                      <Form.Label column sm="3" className="createAccountLabels">First Name</Form.Label>
+                      <Col sm="9" >
+                          <Form.Control 
+                          required
+                          type="text"
+                          name="firstName"
+                          placeholder="First name"
+                          />
+                      </Col>                    
+                  </Form.Group>
+
+                  <Form.Group as={Row} className="mb-3" controlId="validationLastName">
+                      <Form.Label column sm="3" className="createAccountLabels">Last Name</Form.Label>
+                      <Col sm="9">
+                          <Form.Control
+                          required
+                          type="text"
+                          name="lastName"
+                          placeholder="Last name"
+                          />
+                      </Col>                    
+                  </Form.Group>
+
+                  <Form.Group as={Row} className="mb-3" controlId="formBasicEmail">
+                      <Form.Label column sm="3" className="createAccountLabels">Email Address</Form.Label>
+                      <Col sm="9">
+                          <Form.Control type="email" name="email" placeholder="Enter email" required />
+                      </Col>
+                  </Form.Group>
+
+                  <Form.Group as={Row} className="mb-3" controlId="formBasicPassword">
+                      <Form.Label column sm="3" className="createAccountLabels">Create Password</Form.Label>
+                      <Col sm="9">
+                          <Form.Control type="password" name="password" placeholder="Enter password" required />
+                      </Col>
+                  </Form.Group>
+
+                  <Form.Group as={Row} className="mb-3" controlId="validationPhoneNumber">
+                      <Form.Label column sm="3" className="createAccountLabels">Phone Number</Form.Label>
+                      <Col sm="9">
+                          <Form.Control
+                          required
+                          type="number"
+                          name="phoneNumber"
+                          placeholder="Phone Number"
+                          />
+                      </Col>                    
+                  </Form.Group>                  
+
+                  <Form.Group as={Row} className="mb-3" controlId="validationAddress1">
+                      <Form.Label column sm="3" className="createAccountLabels">Address</Form.Label>
+                      <Col sm="9" className="">
+                          <Form.Control type="text" name="address1" placeholder="Address" required />
+                          <Form.Control.Feedback type="invalid">Please provide a valid address.
+                          </Form.Control.Feedback>
+                      </Col>
+                  </Form.Group>
+
+                  <Form.Group as={Row} className="mb-3" controlId="validationAddress2">
+                      <Form.Label column sm="3" className="createAccountLabels">Address 2</Form.Label>
+                      <Col sm="9" className="">
+                          <Form.Control type="text" name="address2" placeholder="Address 2"/>
+                      </Col>
+                  </Form.Group>
+                  
+                  <Form.Group as={Row} className="mb-3" controlId="validationCity">
+                      <Form.Label column sm="3" className="createAccountLabels">City</Form.Label>
+                      <Col sm="9">
+                          <Form.Control type="text" name="city" placeholder="City" required />
+                          <Form.Control.Feedback type="invalid">Please provide a valid city.
+                          </Form.Control.Feedback>
+                      </Col>
+                  </Form.Group>
+
+                  <Form.Group as={Row} className="mb-3" controlId="validationState">
+                      <Form.Label column sm="3" className="createAccountLabels">State</Form.Label>
+                      <Col sm="9">
+                          <Form.Control type="text" name="state1" placeholder="State" required />
+                          <Form.Control.Feedback type="invalid">Please provide a valid state.
+                          </Form.Control.Feedback>
+                      </Col>
+                  </Form.Group>
+
+                  <Form.Group as={Row} className="mb-3" controlId="validationZipcode">
+                      <Form.Label column sm="3" className="createAccountLabels">Zipcode</Form.Label>
+                      <Col sm="5">
+                          <Form.Control type="number" name="zipcode" placeholder="Zipcode" required />
+                          <Form.Control.Feedback type="invalid">Please provide a valid zipcode.
+                          </Form.Control.Feedback>
+                      </Col>
+                  </Form.Group>
+              
+                  <div >
+                      <Form.Group as={Row} className="mb-4">
+                      <Form.Label column sm={3}></Form.Label>
+                          <Col sm={9}>
+                              <Form.Check style={{textAlign: 'left'}}
+                                  required
+                                  label="Agree to terms and conditions"
+                                  feedback="You must agree before submitting."
+                                  feedbackType="invalid"
+                              />
+                          </Col>
+                      </Form.Group>
+                  </div>
+
+                  <Button className="m-4" type="submit" size="lg" style={{display: 'inline-block'}}>Submit</Button>          
+              </Form>
+
+              <div >
+                  <a href="/Login" id="cancel" name="cancel" className="btn btn-danger btn-lg" style={{display: 'inline-block'}}>Cancel</a>
+              </div>         
+          </div>
+     
+          </Row> 
+      </>
+  );
+}
+
+
 
 class CreateAccount extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {value: '', email: "", password: ""};  /* state array */
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {value: '', email: "", password: ""};  /* state array */
+  // }
 
-  /* Handles changes to a account box by updating the appropriate state in this.state */
-  handleChange(name, event) {
-    this.setState({[name]: event.target.value});
-  }
+  // /* Handles changes to a account box by updating the appropriate state in this.state */
+  // handleChange(name, event) {
+  //   this.setState({[name]: event.target.value});
+  // }
 
-  /* Handles the redirect to the login page */
-  handleCancel(event) {
-    this.props.history.go(-1);
-  }
+  // /* Handles the redirect to the login page */
+  // handleCancel(event) {
+  //   this.props.history.go(-1);
+  // }
 
-  /* TODO -- Handles account registration */
-  handleRegister(event) { /* Check if either email or password boxes are blank */
+  // /* TODO -- Handles account registration */
 
-    Axios.post("http://localhost:3001/CreateAccount",{
-    username: this.state.email.trim(),
-    password: this.state.password.trim()
-    }).then((response) => {
-      if (this.state.email.trim().length == 0 || this.state.password.trim().length == 0) { 
-        alert("Please fill out both an email and password.");
-      }
-      else{
-        this.props.history.push('/Login');
-      }
-    });
     
-  }
+  // }
+
+  // handleAccountCreation() {
+
+  // }
+  
+
 
   render() {
     return (
       <>
         <img src="MadRentals_Logo_Light.png" height="auto" width="auto"></img>
-          <FormControl placeholder = "Email" value={this.state.email} onChange={this.handleChange.bind(this, "email")} />
-          <FormControl placeholder = "Password" value={this.state.password} onChange={this.handleChange.bind(this, "password")} />
         <div>
-          <Button onClick = {() => this.handleCancel()}>Cancel</Button>
-          <Button onClick = {() => this.handleRegister()}>Register</Button>
+          <Row>
+            <CreateAccountForm/>
+          </Row>
         </div>   
       </>
     );
--- react-node-app/spike-exercise/src/Login.js
+++ react-node-app/spike-exercise/src/Login.js
@@ -1,9 +1,86 @@
 import React, {useState} from "react"; 
 import Axios from 'axios';
-import Button from "react-bootstrap/Button";
 import FormControl from "react-bootstrap/FormControl"
+import { Form, Button, Row, Col} from "react-bootstrap";
 import {Route, withRouter} from 'react-router-dom'
 import './App.css';
+
+
+function LoginPage() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    
+    setValidated(true);
+      
+    // Output Caputured Data
+    if (form.checkValidity() === true) {
+      console.log(event.target.elements.firstName.value)
+      console.log(event.target.elements.lastName.value)
+      console.log(event.target.elements.password.value)
+      console.log(event.target.elements.phoneNumber.value)
+      console.log(event.target.elements.email.value)
+      console.log(event.target.elements.address1.value)
+      console.log(event.target.elements.address2.value)
+      console.log(event.target.elements.city.value)
+      console.log(event.target.elements.state1.value)
+      console.log(event.target.elements.zipcode.value)
+
+      Axios.post("http://localhost:3001/CreateAccount",{
+        email: this.state.email.trim(),
+        password: this.state.password.trim(),
+        }).then((response) => {
+            this.props.history.push('/Login');
+        });
+      }          
+    };   
+
+  const handleCancel = (event) => {
+      setValidated(true);
+  }
+
+  return (
+      <>          
+          <Row style={{padding: '2%'}}>
+          <div >            
+              <Form noValidate validated={validated} action="RenterHome" onSubmit={handleSubmit} onCancel={handleCancel}>
+                  <Form.Group as={Row} className="mb-3" controlId="validationEmail">
+                      <Form.Label column sm="3" className="createAccountLabels">Email</Form.Label>
+                      <Col sm="9" >
+                          <Form.Control 
+                          required
+                          type="email"
+                          name="email"
+                          placeholder="Email address"
+                          />
+                      </Col>                    
+                  </Form.Group>
+
+                  <Form.Group as={Row} className="mb-3" controlId="validationPassword">
+                      <Form.Label column sm="3" className="createAccountLabels">Password</Form.Label>
+                      <Col sm="9">
+                          <Form.Control
+                          required
+                          type="password"
+                          name="password"
+                          placeholder="Password"
+                          />
+                      </Col>                    
+                  </Form.Group>
+
+                  <Button className="m-4" type="submit"  style={{display: 'inline-block'}}>Submit</Button>          
+              </Form>        
+          </div>
+     
+          </Row> 
+      </>
+  );
+}
 
 class Login extends React.Component {
 
@@ -66,16 +143,21 @@ class Login extends React.Component {
   render() {
     return (
       <>
+      
         <img src="MadRentals_Logo_Light.png" height="auto" width="auto"></img>
-        <FormControl placeholder = "Email" value={this.state.email} onChange={this.handleChange.bind(this, "email")} />
-        <FormControl placeholder = "Password" value={this.state.password}  onChange={this.handleChange.bind(this, "password")} />
-        <div>
-          <Button onClick = {() => this.handleCreateAccount()}>Create Account</Button>
-          <Button onClick = {() => this.handleLogin()}>Login</Button>
-          <Button onClick = {() => this.handleRentalApplicationTEST()}>RenterApplicationTEST</Button>
-          <Button onClick = {() => this.handleRenterPaymentTEST()}>RenterPaymetTEST</Button>
-          <Button onClick = {() => this.handleRenterRequestsTEST()}>RenterRequestsTEST</Button>
-          <Button onClick = {() => this.handleAdminRequestsTEST()}>AdminRequestsTEST</Button>
+
+        <LoginPage />
+
+        {/* <FormControl placeholder = "Email" value={this.state.email} onChange={this.handleChange.bind(this, "email")} /> */}
+        {/* <FormControl placeholder = "Password" value={this.state.password}  onChange={this.handleChange.bind(this, "password")} /> */}
+        <div>          
+          {/* <Button className="my-2" onClick = {() => this.handleLogin()}>Login</Button>&nbsp;&nbsp; */}
+          <Button className="my-2 "onClick = {() => this.handleCreateAccount()}>Create Account</Button>
+          <br />
+          <Button onClick = {() => this.handleRentalApplicationTEST()}>RenterApplicationTEST</Button>&nbsp;
+          <Button onClick = {() => this.handleRenterPaymentTEST()}>RenterPaymetTEST</Button>&nbsp;
+          <Button onClick = {() => this.handleRenterRequestsTEST()}>RenterRequestsTEST</Button>&nbsp;
+          <Button onClick = {() => this.handleAdminRequestsTEST()}>AdminRequestsTEST</Button>&nbsp;
         </div>   
       </>
     );
